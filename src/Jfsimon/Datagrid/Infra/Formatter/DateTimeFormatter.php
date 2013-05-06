186d2b345f52e295caf3aa90281242a09c51dcac
Jean-François Simon <contact@jfsimon.fr>
2013-05-06 09:17:37 +0200

    fixed wrong tests fixtures (local time zone issue)

--- src/Jfsimon/Datagrid/Infra/Formatter/DateTimeFormatter.php
+++ src/Jfsimon/Datagrid/Infra/Formatter/DateTimeFormatter.php
@@ -20,7 +20,7 @@ class DateTimeFormatter implements FormatterInterface
             'null_value'  => '',
             'date_format' => \IntlDateFormatter::MEDIUM,
             'time_format' => \IntlDateFormatter::SHORT,
-            'time_zone'   => 'UTC',
+            'time_zone'   => null,
             'calendar'    => \IntlDateFormatter::GREGORIAN,
             'pattern'     => null,
         ));
